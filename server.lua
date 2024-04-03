358d4241f4970926bf72b3e800ba26a5d7e0c88e
RageOps <Dschneid098@gmail.com>
2024-04-02 22:12:58 -0400

    (fix) Remove capitals from command names so they are easier to type

--- server.lua
+++ server.lua
@@ -6,31 +6,31 @@ RegisterNetEvent('ops-fireworks:server:addCoordToShow', function()
 
 end)
 
-RegisterCommand('lockdownFireworks', function(source, args, raw)
+RegisterCommand('lockdownfireworks', function(source, args, raw)
     TriggerClientEvent('ops-fireworks:client:ReceiveLockdownStatus', -1, true)
 end, true)
 
-RegisterCommand('unlockFireworks', function(source, args, raw)
+RegisterCommand('unlockfireworks', function(source, args, raw)
     TriggerClientEvent('ops-fireworks:client:ReceiveLockdownStatus', -1, false)
 end, true)
 
-RegisterCommand('addCoordToShow', function(source, args, raw)
+RegisterCommand('addcoordtoshow', function(source, args, raw)
     local ped = GetPlayerPed(source)
     local coords = GetEntityCoords(ped)
     table.insert(showCoords, coords)
     TriggerClientEvent('ops-fireworks:server:addCoordToShow', -1, coords)
 end, true)
 
-RegisterCommand('startBigShow', function(source, args, raw)
+RegisterCommand('startbigshow', function(source, args, raw)
     bigShowStarted = true
     TriggerClientEvent('ops-fireworks:server:startBigShow', -1, showCoords)
 end, true)
 
-RegisterCommand('stopBigShow', function(source, args, raw)
+RegisterCommand('stopbigshow', function(source, args, raw)
     bigShowStarted = false
     TriggerClientEvent('ops-fireworks:server:stopBigShow', -1)
 end, true)
 
-RegisterCommand('clearShow', function(source, args, raw)
+RegisterCommand('clearshow', function(source, args, raw)
     showCoords = {}
 end, true)
